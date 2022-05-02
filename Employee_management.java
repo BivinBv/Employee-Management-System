2dea1ceced63420e3aafb02e8789d50bb473eb2e
BivinBv <bivin.b@techversantinfo.com>
2022-05-02 16:26:16 +0530

    Common connection established

--- Employee_management.java
+++ Employee_management.java
@@ -1,9 +1,22 @@
 package emp_management_sys;
-
+import java.sql.*;
+class Empmng
+{
+	private static Connection getconnection() throws Exception
+	{
+		Connection con=null;
+		String url="jdbc:mysql://localhost:3306/emp_management";
+		String uname="root";
+		String pass="root";
+		Class.forName("com.mysql.jdbc.Driver");
+		con=DriverManager.getConnection(url,uname,pass);
+		return con;
+	}
+}
 public class Employee_management {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
